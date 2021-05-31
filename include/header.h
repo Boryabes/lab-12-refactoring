1133227de6e28283ad6ca7946aee90aa9331120b
boryabes <box.bern@yandex.ru>
2021-06-01 02:12:26 +0600

    end

---
+++ include/header.h
@@ -0,0 +1,11 @@
+// Copyright 2021 E6A4 <box.bern@yandex.ru>
+
+#ifndef TEMPLATE_HEADER_H
+#define TEMPLATE_HEADER_H
+#include <string>
+#include <vector>
+#include <iostream>
+using string = std::string;
+using std::cout;
+using std::endl;
+#endif // TEMPLATE_HEADER_H
---
+++ sources/Histogram.cpp
@@ -0,0 +1,22 @@
+// Copyright 2021 E6A4 <box.bern@yandex.ru>
+
+#include "Histogram.h"
+int Histogram::GetNumSkip() const {
+  return num_skip;
+}
+Histogram& Histogram::GetInstance() {
+  static Histogram instance;
+  return instance;
+}
+void Histogram::SetAvg(const float& avg_) {
+  avg = avg_;
+}
+void Histogram::PlusNumSkip() {
+  ++num_skip;
+}
+void Histogram::NewLap() {
+  num_skip = 0;
+}
+float Histogram::GetAvg() const {
+  return avg;
+}
